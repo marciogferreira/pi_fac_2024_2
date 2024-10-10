8f5a40c635e0102de2696911fb33433d37bf9b64
Márcio Ferreira <hmarciogferreira@gmail.com>
2024-10-10 20:47:50 -0300

    Model

---
+++ models/TypeUserMode.js
@@ -0,0 +1,16 @@
+const Connection = require('../config/Connection')
+
+const TypeUserModel = Connection.define(
+    'TypeUserModel',
+    {
+        nome: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        tableName: 'tipo_usuario',
+    }
+)
+
+module.exports = TypeUserModel;
